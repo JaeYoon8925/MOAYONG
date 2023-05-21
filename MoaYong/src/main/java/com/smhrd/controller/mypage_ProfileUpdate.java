9dc5bdb0edbebdf204f58ac4115ad7f194d71170
smhrd <smhrd@121.147.185.76>
2023-05-22 08:56:34 +0900

    누락되었던 콘들

---
+++ MoaYong/src/main/java/com/smhrd/controller/mypage_ProfileUpdate.java
@@ -0,0 +1,75 @@
+package com.smhrd.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.smhrd.dao.ProjectDAO;
+import com.smhrd.dao.UserDAO;
+import com.smhrd.entity.Project;
+import com.smhrd.entity.User;
+
+public class mypage_ProfileUpdate implements controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// 1. 데이터 수집
+		request.setCharacterEncoding("UTF-8");
+
+		String pw = request.getParameter("password");
+		String nickname = request.getParameter("nickname");
+		String self_intro = request.getParameter("self_intro");
+
+		// +) session으로부터 email 값 꺼내오기.
+		HttpSession session = request.getSession();
+		User user = (User) session.getAttribute("user");
+		String t_id = user.getT_id();
+
+		User dto = new User();
+		dto.setT_id(t_id);
+		dto.setPw(pw);
+		dto.setNickname(nickname);
+		dto.setSelf_intro(self_intro);
+
+		// 2. 기능구현
+		UserDAO dao = new UserDAO();
+		int row = dao.profileUpdate(dto);
+		
+
+		// 3. VIEW 선택
+		String nextView = "";
+		if (row > 0) {
+			System.out.println("업데이트 성공");
+			nextView = "mypage";
+			
+			ProjectDAO dao2=new ProjectDAO();
+			
+			List<Project> partinList = dao2.partin(t_id);
+			request.setAttribute("partinList", partinList);
+			
+			List<Project> ingList = dao2.ing(t_id);
+			request.setAttribute("ingList", ingList);
+			
+			List<Project> mytest = (List<Project>)request.getAttribute("ingList");
+			List<Project> endList = dao2.end(t_id);
+			request.setAttribute("endList", endList);
+			List<Project> mytest2 = (List<Project>)request.getAttribute("endList");
+			
+			// session에 저장된 데이터 업데이트
+			session.setAttribute("user", dto);
+		} else {
+			nextView = "goProfileUpdate";
+			System.out.println("업데이트 실패");
+		}
+		
+		return nextView;
+
+	}
+
+}
---
+++ MoaYong/src/main/java/com/smhrd/controller/mypage_goProfileUpdateCon.java
@@ -0,0 +1,24 @@
+package com.smhrd.controller;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.smhrd.dao.BoardDAO;
+import com.smhrd.entity.Board;
+
+public class mypage_goProfileUpdateCon implements controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		return "ProfileUpdate"; // 게시글 보여주는 JSP 파일의 경로
+	}
+
+}
